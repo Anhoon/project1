22bf9dd0aeecb49da20aca58338f109b61ccb0cc
AnHoon <shine_foryou@hanmail.net>
2021-10-26 18:51:58 +0900

    스케줄러 추가

--- src/main/java/kr/co/smartcube/xcube/XcubeApplication.java
+++ src/main/java/kr/co/smartcube/xcube/XcubeApplication.java
@@ -3,9 +3,11 @@ package kr.co.smartcube.xcube;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import kr.co.smartcube.xcube.common.FileUploadProperties;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties({
     FileUploadProperties.class
